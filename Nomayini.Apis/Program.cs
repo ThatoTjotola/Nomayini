437176c24f8a0fadd6989c7b38e6402fd5a0bb04
jimmy <jimmy@JIMMY>
2025-04-28 16:43:20 +0200

    nit adjustments for scalar

--- Nomayini.Apis/Program.cs
+++ Nomayini.Apis/Program.cs
@@ -49,10 +49,10 @@ builder.Services.AddOpenApi(options =>
             }
         };
 
-        //document.Servers = new List<OpenApiServer>
-        //{
-        //    new OpenApiServer { Url = "https://jimmytjotola.org" }
-        //};
+        document.Servers = new List<OpenApiServer>
+        {
+            new OpenApiServer { Url = "https://jimmytjotola.org" }
+        };
 
         document.Components ??= new OpenApiComponents();
         document.Components.SecuritySchemes.Add("Bearer", new OpenApiSecurityScheme
