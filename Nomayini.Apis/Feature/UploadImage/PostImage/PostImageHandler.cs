5fe073bf8b946e4fed460ee819c9f6bd3f7f2b8d
Jimmy Thato Amo Tjotola <124493697+ThatoTjotola@users.noreply.github.com>
2025-04-28 16:39:06 +0200

    Feature/upload image (#4)
    * Adjustments too include image uploads per user after authentication
    
    * NIT ADJUSTMENTS
    
    * NIT ADJUSTMENTS
    
    ---------
    
    Co-authored-by: jimmy <jimmy@JIMMY>

---
+++ Nomayini.Apis/Feature/UploadImage/PostImage/PostImageHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Claims;
+using MediatR;
+
+namespace Nomayini.Apis.Feature.UploadImage.PostImage;
+
+public sealed class PostImageCommandHandler(AppDbContext dbContext, IHttpContextAccessor context) : IRequestHandler<PostImageCommand, Unit>
+{
+    public async Task<Unit> Handle(
+           PostImageCommand command,
+           CancellationToken cancellationToken)
+    {
+        var userId = context.HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value.ToString();
+        string currenTime= DateTime.Now.ToString("HH:mm:ss tt");
+
+        string path = "wwwroot/images";
+
+        bool exists = Directory.Exists(path);
+        if (!exists)
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        var extension = Path.GetExtension(command.image.FileName);
+        var filePath = Path.Combine(path, $"{userId}{extension}");
+
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await command.image.CopyToAsync(stream);
+        }
+
+        return Unit.Value;
+
+    }
+}
+
--- Nomayini.Apis/Program.cs
+++ Nomayini.Apis/Program.cs
@@ -3,14 +3,15 @@ using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Nomayini.Apis.Core.Authentication;
 using Nomayini.Apis.Feature.Auth.Login;
 using Nomayini.Apis.Feature.Auth.Register;
 using Nomayini.Apis.Feature.Messaging.GetMessage;
 using Nomayini.Apis.Feature.Messaging.PostMessage;
+using Nomayini.Apis.Feature.UploadImage.PostImage;
 using Nomayini.Apis.Infrastructure.Middleware;
 using Nomayini.Apis.Shared.Behaviours;
-using Microsoft.OpenApi.Models;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,6 +19,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Register built-in authorization and OpenAPI tools
 builder.Services.AddAuthorization();
 builder.Services.AddHttpContextAccessor();
+builder.Services.AddAntiforgery();
 builder.Services.AddOpenApi(options =>
 {
     options.AddDocumentTransformer((document, context, _) =>
@@ -45,10 +47,10 @@ builder.Services.AddOpenApi(options =>
             }
         };
 
-        document.Servers = new List<OpenApiServer>
-        {
-            new OpenApiServer { Url = "https://jimmytjotola.org" }
-        };
+        //document.Servers = new List<OpenApiServer>
+        //{
+        //    new OpenApiServer { Url = "https://jimmytjotola.org" }
+        //};
 
         document.Components ??= new OpenApiComponents();
         document.Components.SecuritySchemes.Add("Bearer", new OpenApiSecurityScheme
@@ -108,7 +110,9 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 var app = builder.Build();
 
-// Map OpenAPI endpoint);
+// Map OpenAPI endpoint);'
+app.UseRouting();
+app.UseAntiforgery();
 app.MapOpenApi();
 app.MapScalarApiReference(options =>
 {
@@ -126,7 +130,7 @@ RegisterEndpoint.MapEndpoint(app);
 LoginEndpoint.MapEndpoint(app);
 PostMessageEndpoint.MapEndpoint(app);
 GetAllMessagesEndpoint.MapEndpoint(app);
-
+PostImageEndpoint.MapEndpoint(app);
 
 // Database initialization
 using (var scope = app.Services.CreateScope())
