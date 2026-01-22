a6ab0fdabf4df1eb1916997e929bab2d8c10078b
JIMMY <tjotolajimmy@gmail.com>
2026-01-22 20:41:44 +0200

    Producer service

--- Portfolio.Producer/Program.cs
+++ Portfolio.Producer/Program.cs
@@ -1,6 +1,7 @@
 using Portfolio.Producer;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddSingleton<IKafkaProducerService, KafkaProducerService>();
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
--- Portfolio.Producer/Worker.cs
+++ Portfolio.Producer/Worker.cs
@@ -1,7 +1,9 @@
+using Confluent.Kafka;
+
 namespace Portfolio.Producer
 {
     //Gonna delete the following class later replace with a producer for RabbitMQ 
-    public class Worker(ILogger<Worker> logger) : BackgroundService
+    public class Worker(ILogger<Worker> logger, IKafkaProducerService producerService) : BackgroundService
     {
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
@@ -9,7 +11,10 @@ namespace Portfolio.Producer
             {
                 if (logger.IsEnabled(LogLevel.Information))
                 {
-                    logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                        Random ran = new Random();
+                        var nextString = ran.Next(10000);
+                        await producerService.SendMessageAsync("reaching-out", nextString.ToString());
+                        logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 }
                 await Task.Delay(1000, stoppingToken);
             }
