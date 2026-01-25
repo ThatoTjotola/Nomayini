ab300a0cb1ab878977ba50762afc5c979de2d17f
JIMMY <tjotolajimmy@gmail.com>
2026-01-25 18:13:48 +0200

    Adjusts for worker

--- Notification.Consumer/KafkaConsumerServices/KafkaConsumerService.cs
+++ Notification.Consumer/KafkaConsumerServices/KafkaConsumerService.cs
@@ -4,6 +4,7 @@ namespace Notification.Consumer.KafkaConsumerServices;
 public class KafkaConsumerService : IKafkaConsumerService
 {
     private readonly IConsumer<Null, string> _consumer;
+    //Gotta think about making this a singleton in the future and inject service factory here
     private readonly IEmailService _emailService;
 
     public KafkaConsumerService(IEmailService emailService, IConfiguration configuration)
@@ -11,7 +12,6 @@ public class KafkaConsumerService : IKafkaConsumerService
         var bootstrapServers = configuration["Kafka:BootstrapServers"] ?? "localhost:9092";
         var config = new ConsumerConfig
         {
-            //192.168.8.101:9092
             BootstrapServers = bootstrapServers,
             GroupId = "my-notification-group",
             AutoOffsetReset = AutoOffsetReset.Earliest
