d47059ad337616aa1e11199d9274e5133a245d6f
JIMMY <tjotolajimmy@gmail.com>
2026-02-08 13:52:52 +0200

    Adjustments for feature flag

--- Notification.Consumer/KafkaConsumerServices/KafkaConsumerService.cs
+++ Notification.Consumer/KafkaConsumerServices/KafkaConsumerService.cs
@@ -9,10 +9,12 @@ public class KafkaConsumerService : IKafkaConsumerService
     //Gotta think about making this a singleton in the future and inject service factory here
     private readonly IEmailService _emailService;
     private readonly NotifyerSettings _notifyerSettings;
+    private readonly bool? _notificationToggle;
 
     public KafkaConsumerService(IEmailService emailService, IConfiguration configuration, IOptions<NotifyerSettings> notifyerSettings)
     {
         var bootstrapServers = configuration["Kafka:BootstrapServers"] ?? "localhost:9092";
+        var notificationToggle = configuration["NotifyerSettings:EnableEmailSendTrue"];
         var config = new ConsumerConfig
         {
             BootstrapServers = bootstrapServers,
@@ -23,6 +25,7 @@ public class KafkaConsumerService : IKafkaConsumerService
         _consumer = new ConsumerBuilder<Null, string>(config).Build();
         _emailService = emailService;
         _notifyerSettings = notifyerSettings.Value;
+        _notificationToggle = !string.IsNullOrEmpty(notificationToggle) ? bool.Parse(notificationToggle) : (bool?)null;
     }
 
     public async Task ConsumeMessages(string topic)
@@ -37,7 +40,7 @@ public class KafkaConsumerService : IKafkaConsumerService
                 Console.WriteLine($"Consumed message: {consumeResult.Message.Value}");
                 _consumer.Commit(consumeResult);
                 //send email after consumption 
-                if (_notifyerSettings.EnableEmailSend)
+                if (_notificationToggle ?? _notifyerSettings.EnableEmailSend)
                 {
                     await _emailService.SendEmail(consumeResult.Message.Value.ToString(), consumeResult.Message.Value.ToString());
                 }
